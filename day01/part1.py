d950bc4c03ea06016ffac36ed44d596872847844
Dimitar Rusev <mitikodev@gmail.com>
2022-12-02 02:15:18 +0200

    There're no negative calories

--- day01/part1.py
+++ day01/part1.py
@@ -2,12 +2,12 @@
 
 input = open("input.txt").readlines()
 
-max_calories = None
+max_calories = 0
 accum_calories = 0
 
 for line in input:
     if line == "\n":
-        if max_calories == None or accum_calories > max_calories:
+        if accum_calories > max_calories:
             max_calories = accum_calories
         accum_calories = 0
         continue
@@ -15,7 +15,7 @@ for line in input:
     calories = int(line)
     accum_calories += calories
 
-if max_calories == None or accum_calories > max_calories:
+if accum_calories > max_calories:
     max_calories = accum_calories
 
 print(max_calories)
