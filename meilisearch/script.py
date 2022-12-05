0b5e9cf56bff6aefeb6eb055538821fa2c245d78
Dimitar Rusev <mitikodev@gmail.com>
2022-12-05 12:40:16 +0200

    PoW

--- meilisearch/script.py
+++ meilisearch/script.py
@@ -7,130 +7,116 @@ lines = open(filename).read().strip().split('\n')
 kid_paths = [[x.strip() for x in line.split('-')] for line in lines]
 kids = [x[0] for x in kid_paths]
 paths = [x[1] for x in kid_paths]
-# paths = [''.join(['1' if (ord(c)-ord('L')) > 0 else '0' for c in path]) for path in paths]
 
 def lcp(a, b):
     l = 0
     for i in range(min(len(a), len(b))):
-        if a[i] == b[i]:
-            l += 1
-        else:
-            return l
+        if a[i] == b[i]: l += 1
+        else: return l
     return l
 
+def slcp(a, b):
+    if len(a) == 0 or len(b) == 0:
+        return 0
+    return int(a[0] == b[0])
+
 class Node:
-    def __init__(self, data=None, left=None, right=None, index=-1):
+    def __init__(self, data, left=None, right=None, index=-1):
         self.left = left
         self.right = right
         self.data = data
         self.index = index
     
     def insert(self, data, index):
+        # print(f"Inserting {data} in {self.data}, idx={self.index}")
+        l = lcp(self.data, data)
+        radix = data[:l]
+        node = Node(data[l:], index=index)
+
+        if len(node.data) == 0:
+            if self.index == -1:
+                self.index = node.index
+            return
+
+        # if no children, then either appaned down, or do a switcheroo
         if self.left == None:
-            if self.data == None:
-                self.left = Node(data, index=index)
-                return
+            assert self.index != -1
 
-            l = lcp(self.data, data)
-            # assert l > 0
             if l == len(self.data):
-                self.left = Node(self.data[l:], index=self.index)
-                self.right = Node(data, index=index)
-                self.index = -1
-            else:
-                node_a = Node(data[l:], index=index)
-                node_b = Node(self.data[l:], index=self.index)
-                self.index = -1; self.data = data[:l]
-                if node_a.data < node_b.data:
-                    self.left, self.right = node_a, node_b
-                else:
-                    self.left, self.right = node_b, node_a
-            return
-        if self.right == None:
-            l = lcp(self.left.data, data)
-            if l == 0:
-                if self.left.data < data:
-                    self.right = Node(data, index=index)
-                else:
-                    self.right = self.left
-                    self.left = Node(data, index=index)
+                self.left = node
                 return
-            if self.left.index == -1:
-                if l == len(self.left.data):
-                    return self.left.insert(data[l:], index)
-                else:
-                    radix = data[:l]
-                    node = Node(data[l:], index=index)
-                    self.left.data = self.left.data[l:]
 
-                    if self.left.data < node.data:
-                        self.left = Node(radix, self.left, node)
-                    else:
-                        self.left = Node(radix, node, self.left)
-                    return
+            modi = Node(self.data[l:], index=self.index)
+            self.data = radix; self.index = -1
 
-            radix = data[:l]
-            node_a = Node(self.left.data[l:], index=self.left.index)
-            node_b = Node(data[l:], index=index)
-            if node_a.data < node_b.data:
-                self.left = Node(radix, node_a, node_b)
+            if modi.data < node.data:
+                self.left, self.right = modi, node
             else:
-                self.left = Node(radix, node_b, node_a)
+                self.left, self.right = node, modi
+            return
+        
+        # if one child, then either append to left, append to right, or switcheroo
+        if self.right == None:
+            assert self.index != -1
+
+            if l == len(self.data):
+                if slcp(self.left.data, node.data) == 0:
+                    self.right = node
+                    if self.right.data < self.left.data:
+                        self.left, self.right = self.right, self.left
+                else:
+                    self.left.insert(node.data, node.index)
+                return
+
+            modi = Node(self.data[l:], left=self.left, index=self.index)
+            self.data = radix; self.index = -1
+
+            if modi.data < node.data:
+                self.left, self.right = modi, node
+            else:
+                self.left, self.right = node, modi
             return
 
-        assert self.index == -1
-        # maybe?
-        # assert(len(self.left.data) > 0 and len(self.right.data) > 0)
-
-        # find if we should insert under left or right
-        l0 = lcp(self.left.data, data)
-        l1 = lcp(self.right.data, data)
-        if l0 > l1:
-            if l0 == len(self.left.data) and self.left.index == -1:
-                self.left.insert(data[l0:], index)
+        if l == len(self.data):
+            ll = slcp(self.left.data, node.data)
+            lr = slcp(self.right.data, node.data)
+            if lr == 0:
+                if ll == 0:
+                    if self.index != -1:
+                        self.index = self.left.index
+                    self.left = node
+                    return
+                self.left.insert(node.data, node.index)
+            elif ll == 0:
+                self.right.insert(node.data, node.index)
             else:
-                radix = data[:l0]
-                node_a = Node(data[l0:], index=index)
-                node_b = Node(self.left.data[l0:], index=self.left.index)
-                if node_a.data < node_b.data:
-                    self.left = Node(radix, node_a, node_b)
-                else:
-                    self.left = Node(radix, node_b, node_a)
-        elif l0 < l1:
-            if l1 == len(self.right.data) and self.right.index == -1:
-                self.right.insert(data[l1:], index)
-            else:
-                # rem = "" if self.data == None else self.data[l1:]
-                # node_b = Node(rem, index=self.right.index)
-                radix = data[:l1]
-                node_a = Node(data[l1:], index=index)
-                node_b = Node(self.right.data[l1:], index=self.right.index)
-                if node_a.data < node_b.data:
-                    self.right = Node(radix, node_a, node_b)
-                else:
-                    self.right = Node(radix, node_b, node_a)
-        elif l0 == 0 and l1 == 0:
+                print("Unreachable")
+                assert False
+            return
 
-            pass
+        modi = Node(self.data[l:], self.left, self.right, self.index)
+        self.data = radix; self.index = -1
+
+        if modi.data < node.data:
+            self.left, self.right = modi, node
         else:
-            print("Unreachable")
-            assert False
+            self.left, self.right = node, modi
+
+
     
     # def walk(self, term=False):
     def walk(self):
-        if self.index != -1:
-            print("Started at non root")
-        
         bfs = [(0, self.left), (0, self.right)]
         # bfs = [self.left, self.right]
         while len(bfs) != 0:
-            # node = bfs.pop() # for dfs
+            # depth, node = bfs.pop() # for dfs
             depth, node = bfs[0]; bfs = bfs[1:]
             # node = bfs[0]; bfs = bfs[1:]
             app = "-"*depth
-            if node != None and node.data != None:
-                print(f"{node.index :>2} {app}{node.data}")
             if node != None:
+                # print(node, node.left, node.right)
+                # dd = node.data if len(node.data) > 0 else "x"
+                # print(f"{node.index :>2} {app}{dd}")
                 # if node.index != -1 and term == True:
                 if node.index != -1:
                     return node.index
@@ -142,12 +128,13 @@ class Node:
 
 # radix_tree = Node(data="root", left=Node("l", left=Node("ll")), right=Node("r", left=Node("rl", right=Node("rlr", index=2))))
 
-radix_tree = Node()
+radix_tree = Node("", index=len(paths))
 for i, path in enumerate(paths):
     # print()
     # print(paths)
     # print(f"Adding '{path}'")
     radix_tree.insert(path, i)
+    # radix_tree.walk()
     # radix_tree.walk()
 
 idx = radix_tree.walk()
