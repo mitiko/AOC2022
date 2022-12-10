2e6b2c3ea5c7d5902d094038e2eeb5f6077d562c
Dimitar Rusev <mitikodev@gmail.com>
2022-12-10 08:05:00 +0200

    Refactor CPU code into its own class

--- day10/part1.py
+++ day10/part1.py
@@ -4,35 +4,35 @@ read_sample = 0
 filename = ["input.txt", "sample.txt"][read_sample]
 lines = open(filename).read().strip().split('\n')
 
+class Machine:
+    def __init__(self):
+        self.cycles = 0
+        self.reg = 1
+
+    def exec(self, func):
+        if func != None:
+            func(self)
+    
+    def run(self, command, func = None):
+        ops = command.split(" ")
+        instr = ops[0]
+        if instr == "noop":
+            self.cycles += 1; self.exec(func)
+        elif instr == "addx":
+            self.cycles += 1; self.exec(func)
+            self.cycles += 1; self.exec(func)
+            self.reg += int(ops[1])
+        else:
+            print("Unrecognized instruction")
+
 total = 0
-cycles = 0
-reg = 1
-done = set()
-
-def do_check(c, r):
+def interim(m: Machine):
     global total
-    for kk in [20, 60, 100, 140, 180, 220]:
-        if kk in done: continue
-        if c >= kk:
-            total += kk*r
-            done.add(kk)
-            break
+    if m.cycles in [20, 60, 100, 140, 180, 220]:
+        total += m.cycles * m.reg
 
+m = Machine()
 for line in lines:
-    opperands = line.split(" ")
-    instr = opperands[0]
-
-    if instr == "noop":
-        cycles += 1
-    elif instr == "addx":
-        cycles += 1
-    do_check(cycles, reg)
-
-    if instr == "addx":
-        cycles += 1
-        do_check(cycles, reg)
-    if instr == "addx":
-        V = int(opperands[1])
-        reg += V
+    m.run(line, interim)
 
 print(total)
--- day10/part2.py
+++ day10/part2.py
@@ -4,48 +4,49 @@ read_sample = 0
 filename = ["input.txt", "sample.txt"][read_sample]
 lines = open(filename).read().strip().split('\n')
 
-cycles = 0
-reg = 1
-# crt = [[0]*40 for _ in range(6)]
-crt = [0]*240
-pos = 0
+class Machine:
+    def __init__(self):
+        self.cycles = 0
+        self.reg = 1
+        self.crt = [0] * 240
 
-def render(reg):
-    global crt
-    global pos
-    sprite = [reg-1, reg, reg+1]
+    def exec(self, func):
+        self.draw()
+        if func != None:
+            func(self)
+    
+    def draw(self):
+        sprite = [self.reg - 1, self.reg, self.reg + 1]
+        pos = self.cycles - 1
+        if pos % 40 in sprite:
+            self.crt[pos] = 1
+        else:
+            self.crt[pos] = 0
+    
+    def render(self):
+        for i in range(6):
+            for j in range(40):
+                pixel = self.crt[i * 40 + j]
+                if pixel == 1:
+                    print('#', end="")
+                else:
+                    print('.', end="")
+            print()
 
-    if pos%40 in sprite:
-        crt[pos] = 1
-    else:
-        crt[pos] = 0
+    def run(self, command, func = None):
+        ops = command.split(" ")
+        instr = ops[0]
+        if instr == "noop":
+            self.cycles += 1; self.exec(func)
+        elif instr == "addx":
+            self.cycles += 1; self.exec(func)
+            self.cycles += 1; self.exec(func)
+            self.reg += int(ops[1])
+        else:
+            print("Unrecognized instruction")
 
-    pos += 1
-
+m = Machine()
 for line in lines:
-    opperands = line.split(" ")
+    m.run(line)
 
-    instr = opperands[0]
-    val = reg
-
-    if instr == "noop":
-        cycles += 1
-    elif instr == "addx":
-        cycles += 1
-
-    render(reg)
-
-    if instr == "addx":
-        cycles += 1
-        render(reg)
-        V = int(opperands[1])
-        reg += V
-
-for i in range(6):
-    for j in range(40):
-        pixel = crt[i*40 + j]
-        if pixel == 0:
-            print('.', end="")
-        elif pixel == 1:
-            print('#', end="")
-    print()
+m.render()
